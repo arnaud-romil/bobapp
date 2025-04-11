dd79252e5b42ebb574cc87397548360486e18e5f
Arnaud ROMIL <arnaud.romil@gmail.com>
2025-04-11 14:57:28 +0200

    back - add failing test

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.bobapp;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failingTest() {
+		assertFalse(true, "This test is designed to fail");
 	}
 
 }
